6b6cdec14fb95bd957fba6cbd43d221be5bcc954
marcus-andre <marcus-andre@hotmail.com>
2025-12-31 16:22:04 -0300

    style: enable static collection for production UI

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -1,8 +1,7 @@
 """
 URL configuration for restaurant_booking project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+The `urlpatterns` list routes URLs to views.
 Examples:
 Function views
     1. Add an import:  from my_app import views
